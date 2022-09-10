3205b67b1cae570c2d3f63c50927aac657afef68
Rana Satpalsinh <85938637+Satpal777@users.noreply.github.com>
2022-09-10 16:53:09 +0530

    Delete Backend directory

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = [YOUR_MONOGDB_URL]
-
-const connectToDb = () => {
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected Successfully")
-    })
-}
-module.exports = connectToDb
